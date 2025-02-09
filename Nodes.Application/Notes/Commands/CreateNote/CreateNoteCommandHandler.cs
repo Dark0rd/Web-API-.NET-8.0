776ceb8f1c26ce7c3c1fba3d16ee015498da62e9
darkord <bleakwhitemol@gmail.com>
2025-02-10 00:54:19 +0300

    Добавьте файлы проекта.

---
+++ Nodes.Application/Notes/Commands/CreateNote/CreateNoteCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Notes.Domain;
+using Notes.Application.interfaces;
+using System.Runtime.CompilerServices;
+using Microsoft.EntityFrameworkCore;
+namespace Notes.Application.Notes.Commands.CreateNote
+{
+    public class CreateNoteCommandHandler : IRequestHandler<CreateNoteCommands, Guid>
+    {
+        private readonly INotesDbContext dbContext;
+        public CreateNoteCommandHandler(INotesDbContext dbContext) => dbContext = dbContext;
+        public async Task<Guid> Handle(CreateNoteCommands request, CancellationToken cancellationToken)
+        {
+            var note = new Note
+            {
+                UserId = request.UserId,
+                Title = request.Title,
+                Details = request.Details,
+                Id = Guid.NewGuid(),
+                CreationDate = DateTime.Now,
+                EditDate = null
+
+            };
+
+           await dbContext.Notes.AddAsync(note, cancellationToken);
+            await dbContext.SaveChangesAsync(cancellationToken);
+            return note.Id;
+            
+        }
+    }
+}
---
+++ Nodes.Application/interfaces/INotesDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+namespace Notes.Application.interfaces
+{
+    public interface INotesDbContext
+    {
+        DbSet<Note> Notes { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+
+}
